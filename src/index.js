917b5868a076987af26a76b8b41f284f261a11f9
ijestanoff <jestanoff@icloud.com>
2024-06-09 19:45:33 +0300

    Fix create movie form

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+
 const routes = require('./routes');
+const configHandlebars = require('./config/configHandkebars');
+const configExpress = require('./config/configExpress');
 
 const app = express();
 const port = 5000;
 
+configHandlebars(app);
+configExpress(app);
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
