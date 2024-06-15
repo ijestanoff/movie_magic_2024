6b39d52015bf9d274200a43ed6739cee4ce6a5b4
ijestanoff <jestanoff@icloud.com>
2024-06-15 18:50:02 +0300

    Refactor movieservice

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,22 +1,10 @@
 const Movie = require('../models/Movie');
 
-const movies = [{
-    _id: 1,
-    title: 'Jungle Cuise',
-    genre: 'Adventure',
-    director: 'Spilberg',
-    year: '2024',
-    imageUrl: '/img/jungle-cruise.jpeg',
-    rating: 5,
-    description: 'Dreaming about saving countless lives and having another adventure, ..',
-}];
+exports.getAll = () => Movie.find();
 
-exports.getAll = () => {
-    return movies.slice();
-};
-
-exports.search = (title, genre, year) => {
-    let result = movies.slice();
+//TODO: filter resolved in DB
+exports.search = async (title, genre, year) => {
+    let result = await Movie.find().lean();
 
     if (title) {
         result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
@@ -33,13 +21,6 @@ exports.search = (title, genre, year) => {
     return result;
 };
 
-exports.getOne = (movieId) => {
-    const movie = movies.find(movie => movie._id == movieId);
-    return movie;
-};
+exports.getOne = movieId => Movie.findById(movieId);
 
-exports.create = (movieData) => {
-    return Movie.create(movieData); 
-    // movieData._id = movies[movies.length - 1]._id + 1;
-    // movies.push(movieData);
-};
+exports.create = movieData => Movie.create(movieData);
